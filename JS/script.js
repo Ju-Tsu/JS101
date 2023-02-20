0440147040dfc77d29d9445778a84512da1186e9
AKIRA <example@gmail.com>
2023-02-20 15:49:21 +0400

    first commit

---
+++ JS/script.js
@@ -0,0 +1,46 @@
+"use strict";
+
+let incr = 10,
+    decr = 10;
+
+// ++incr;
+// --decr;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+
+console.log(2*4 === '8');
+
+const isChecked = true,
+      isClose = false;
+console.log(isChecked || !isClose);
+
+console.log(2 + 2 * 2 != 8);
+
+
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = 'Ivan';
+
+// alert(`Zdarova, ${user}, zaebal`)
+// alert('LoL');
+
+// const result = confirm('Are you here?');
+// console.log(result);
+
+// const answer = prompt('A?', 'Pizda');
+// console.log(typeof(answer));
+
+// const answers = [];
+
+// answers[0] = prompt('a?','');
+// answers[1] = prompt('Pizda','');
+// answers[2] = prompt('a?','');
+// answers[1] = prompt('Pizda','');
+
+// console.log(typeof(answers));

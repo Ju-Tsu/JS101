0d065ea9dff6844003a12f2ff5158ed8e250a6ad
AKIRA <example@gmail.com>
2023-02-20 16:40:29 +0400

    firs console app created

--- JS/script.js
+++ JS/script.js
@@ -1,46 +1,46 @@
-"use strict";
+/* Задание на урок:
 
-let incr = 10,
-    decr = 10;
-
-// ++incr;
-// --decr;
-
-console.log(++incr);
-console.log(--decr);
-
-console.log(5%2);
-
-console.log(2*4 === '8');
-
-const isChecked = true,
-      isClose = false;
-console.log(isChecked || !isClose);
-
-console.log(2 + 2 * 2 != 8);
+1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
+'Сколько фильмов вы уже посмотрели?'
 
 
 
-// const category = 'toys';
+2) Создать объект personalMovieDB и в него поместить такие свойства:
+    - count - сюда передается ответ на первый вопрос
+    - movies - в это свойство поместить пустой объект
+    - actors - тоже поместить пустой объект
+    - genres - сюда поместить пустой массив
+    - privat - в это свойство поместить boolean(логическое) значение false
 
-// console.log(`https://someurl.com/${category}/5`);
+3) Задайте пользователю по два раза вопросы:
+    - 'Один из последних просмотренных фильмов?'
+    - 'На сколько оцените его?'
+Ответы стоит поместить в отдельные переменные
+Записать ответы в объект movies в формате: 
+    movies: {
+        'logan': '8.1'
+    }
 
-// const user = 'Ivan';
+Проверить, чтобы все работало без ошибок в консоли */
 
-// alert(`Zdarova, ${user}, zaebal`)
-// alert('LoL');
+'use strict';
 
-// const result = confirm('Are you here?');
-// console.log(result);
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-// const answer = prompt('A?', 'Pizda');
-// console.log(typeof(answer));
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-// const answers = [];
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
 
-// answers[0] = prompt('a?','');
-// answers[1] = prompt('Pizda','');
-// answers[2] = prompt('a?','');
-// answers[1] = prompt('Pizda','');
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
-// console.log(typeof(answers));
+console.log(personalMovieDB);
